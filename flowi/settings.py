35eccc27854fb73622e3e92a83eda8cc7e8f2132
Nor Berisha <norberisha@proton.me>
2025-12-11 00:28:06 +0100

    Domain

--- flowi/settings.py
+++ flowi/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['flowitodo.live', 'www.flowitodo.live','127.0.0.1', 'localhost']
 
 
 # Application definition
